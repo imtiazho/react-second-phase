5311c0f9d513ce6af1277684459e5023f50698fe
imtiazho <axubair9@gmail.com>
2026-03-14 04:08:31 +0600

    Complete firebase signout method practice

--- simple-firebase-auth/src/Components/Login/Login.jsx
+++ simple-firebase-auth/src/Components/Login/Login.jsx
@@ -1,23 +1,43 @@
-import React from "react";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React, { useState } from "react";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../../Firebase/firebase.init";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log(result);
+        setUser(result.user);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div>
-      <button onClick={handleGoogleSignIn}>Sign in with google</button>
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Sign in with google</button>
+      )}
+
+      {user && (
+        <div>
+          <h1>{user.displayName}</h1>
+          <h1>{user.email}</h1>
+        </div>
+      )}
     </div>
   );
 };
